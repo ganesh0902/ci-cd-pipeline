0a66326d8a2dffcc6dd2bafbc3833ca7035bc88a
Ganesh sakhare <101263428+ganesh0902@users.noreply.github.com>
2025-03-09 14:33:59 +0530

    changes

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
 
     public static void main(String str[])
     {
-        System.out.println("This is Java programs ");   
+        System.out.println("This is Java programs ");  
+        System.out.println("This is Java programs  file");   
     }
 }
